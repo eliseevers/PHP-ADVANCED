a5f697c1c04c33cee480aa05ae67cb2eca78f0be
eliseevers <2155620@talnet.nl>
2022-02-02 10:30:37 +0100

    opdracht 1

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$Elise = array ("voornaam" => "Elise", "achternaam" => "Evers", "klas" => "1A", "leeftijd" => 16);
+echo $Elise ["voornaam"];
+echo $Elise ["achternaam"];
+echo $Elise ["klas"];
+echo $Elise ["leeftijd"];
+
+?>
+</body>
+</html> 
