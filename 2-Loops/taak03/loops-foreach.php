f4a8e93f866e2ff907b95214e3d4929f0dc34ae5
eliseevers <2155620@talnet.nl>
2022-02-07 11:37:33 +0100

    opdracht foreach

---
+++ 2-Loops/taak03/loops-foreach.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$dagen = array("maandag", "dinsadg", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+foreach($dagen as $lijstdagen){
+    echo "<ul>" . "<li>" . $lijstdagen . "</li>" . "</ul>";
+}
+
+?>
+</body>
+</html> 
