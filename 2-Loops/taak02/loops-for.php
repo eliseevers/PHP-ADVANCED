2b8cd8acc31863fa7ef3557f25d69080861a4e29
eliseevers <2155620@talnet.nl>
2022-02-07 11:19:46 +0100

    opdracht4

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+for($x=0; $x<18; $x++){
+    echo "<ul>" . "<li>" . "ik ben $x jaar, dus ik mag nog niet stemmen" . "</li>" . "</ul>";
+}
+echo "<ul>" . "<li>" . "ik ben 18 jaar, dus ik heb stem recht" . "</li>" . "</ul>";
+
+?>
+</body>
+</html> 
