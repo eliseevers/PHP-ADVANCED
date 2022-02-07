e372e422c5653e4dee367126fc53dbd08d783fb2
eliseevers <2155620@talnet.nl>
2022-02-07 09:29:34 +0100

    opdracht 2

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$persoon ["voornaam"]= "Elise";
+$persoon ["achternaam"]= "Evers";
+$persoon ["leeftijd"]=16;
+$persoon ["opleiding"]= "Software Developer";
+
+echo $persoon ['voornaam'] ." " .  $persoon ['achternaam'] . " is " . $persoon ['leeftijd'] . " jaar oud en is student aan het ROC van Amsterdam aan de opleiding " . $persoon ['opleiding'];
+
+
+?>
+</body>
+</html> 
