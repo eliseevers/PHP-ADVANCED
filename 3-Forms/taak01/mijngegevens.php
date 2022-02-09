16caa793317d8945e7c734765641b0bd50c8bd6b
eliseevers <2155620@talnet.nl>
2022-02-09 17:36:07 +0100

    froms post opdracht 2

--- 3-Forms/taak01/mijngegevens.php
+++ 3-Forms/taak01/mijngegevens.php
@@ -1,15 +1,35 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+<style>
+  body {
+    background-color: <?= $_POST["kleuren"] ?>;
+  }
+</style>
+</head>
 <body>
-
 <?php
-echo "<ul>" . "<li>" . "voornaam is " . $_POST["voornaam"] .  "</li>" . "</ul>";
-echo "<ul>" . "<li>" . "achternaam is " . $_POST["achternaam"] . "</li>" . "</ul>";
-echo "<ul>" . "<li>" . "leeftijd is " . $_POST["leeftijd"] . "</li>" . "</ul>";
-echo "<ul>" . "<li>" . "zit in klas " . $_POST["klas"] . "</li>" . "</ul>";
-echo "<ul>" . "<li>" . "adres is " . $_POST["adres"] . "</li>" . "</ul>";
-echo "<ul>" . "<li>" . "woont in " . $_POST["plaatsnaam"] . "</li>" . "</ul>";
-echo "<ul>" . "<li>" . "favoriete muziekband is " . $_POST["favorietemuziekband"] . "</li>" . "</ul>";
+  echo "voornaam:". $_POST["voornaam"];
+  echo "<br>";
+  echo "achternaam:". $_POST["achternaam"];
+  echo "<br>";
+  echo "klas:". $_POST["klas"];
+  echo "<br>";
+  echo "leeftijd:". $_POST["leeftijd"];
+  echo "<br>";
+  echo "adres:". $_POST["adres"];
+  echo "<br>";
+  echo "plaatsnaam:". $_POST["plaatsnaam"];
+  echo "<br>";
+  echo "favorietemuziekband:". $_POST["favorietemuziekband"];
+  echo "<br>";
+  echo "geslacht:" . $_POST["geslacht"];
+  echo "<br/>";
+  echo "kleuren:". $_POST["kleuren"];
 ?>
 </body>
-</html>  
+</html> 
