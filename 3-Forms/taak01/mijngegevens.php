72030e93a26e5efcfc19e6d624ebe869e1ebe9c3
eliseevers <2155620@talnet.nl>
2022-02-09 10:58:10 +0100

    froms opdracht 1

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<ul>" . "<li>" . "voornaam is " . $_POST["voornaam"] .  "</li>" . "</ul>";
+echo "<ul>" . "<li>" . "achternaam is " . $_POST["achternaam"] . "</li>" . "</ul>";
+echo "<ul>" . "<li>" . "leeftijd is " . $_POST["leeftijd"] . "</li>" . "</ul>";
+echo "<ul>" . "<li>" . "zit in klas " . $_POST["klas"] . "</li>" . "</ul>";
+echo "<ul>" . "<li>" . "adres is " . $_POST["adres"] . "</li>" . "</ul>";
+echo "<ul>" . "<li>" . "woont in " . $_POST["plaatsnaam"] . "</li>" . "</ul>";
+echo "<ul>" . "<li>" . "favoriete muziekband is " . $_POST["favorietemuziekband"] . "</li>" . "</ul>";
+?>
+</body>
+</html>  
