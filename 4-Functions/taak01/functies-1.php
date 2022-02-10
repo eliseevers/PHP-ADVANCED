6d7a6dec1529997a3c303dc5ec918725d4a30626
eliseevers <2155620@talnet.nl>
2022-02-10 11:26:38 +0100

    functies 1

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    function schrijfKleur($colorstyle){
+        echo "<li>" . "<font color = $colorstyle>" . "deze tekst is  $colorstyle" . "<li>";
+    }
+    schrijfKleur('red');
+    schrijfKleur('black'); 
+    schrijfKleur('yellow');
+    schrijfKleur('pink');
+    ?>
+</body>
+</html>
