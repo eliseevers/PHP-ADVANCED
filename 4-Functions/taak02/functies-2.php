219dd4408870d889810924cbd45b16e104c0998b
eliseevers <2155620@talnet.nl>
2022-03-09 13:49:09 +0100

    update 4.2

--- 4-Functions/taak02/functies-2.php
+++ 4-Functions/taak02/functies-2.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Functies-2.php</title>
 </head>
 <body>
     <?php
-    function rekenUit($getal1 , $getal2 , $getal3){
-        $som = $getal1+$getal2+$getal3;
-        return $som;
-    }
-    $antwoord=rekenUit(1 , 2 , 3);
-    echo "<li>" ."de uitkomst van deze som is $antwoord". "</li>";
+        function rekenuit ($argument1, $argument2, $argument3){
+            $allesbijelkaar = $argument1 + $argument2 + $argument3;
 
-    $antwoord=rekenUit(7, 8 , 9);
-    echo "<li>" ."de uitkomst van deze som is $antwoord". "</li>";
-
-    $antwoord=rekenUit(10,10,10);
-    echo "<li>" ."de uitkomst van deze som is $antwoord". "</li>";
+            return $allesbijelkaar;
+        }
+        
+        $som1 = rekenuit(2, 8, 9);
+        $som2 = rekenuit(9, 1, 20);
+        $som3 = rekenuit (10, 30, 5);
+        echo "2 + 8 + 9 = $som1<br>";
+        echo "9 + 1 + 20 = $som2<br>";
+        echo "10 + 30 + 5 = $som3<br>";
     ?>
 </body>
 </html>
